0e655a92c3a4c8e8f7513d7dab28f1b3feaa0565
ziyan-cell <ziyanashif01@gmail.com>
2026-06-04 16:30:46 +0530

    Add basic calculator functionality in calculator.py

---
+++ calculator.py
@@ -0,0 +1,7 @@
+num1 = float(input("Enter first number: "))
+num2 = float(input("Enter second number: "))
+
+print("Addition:", num1 + num2)
+print("Subtraction:", num1 - num2)
+print("Multiplication:", num1 * num2)
+print("Division:", num1 / num2)
